3944c114359bbf288f9aec7da8446d75ba0f40a9
ghaash <daniel.silverman@me.com>
2017-04-05 20:15:17 -0700

    add tag scaffolding for nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tags
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
